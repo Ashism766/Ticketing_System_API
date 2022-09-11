444b5459d31c8b4b3a9af87ce1ea968823aeb035
AshisMaity <maity.ashis651@gmail.com>
2022-09-11 11:34:51 +0530

    third attem first commit

---
+++ App.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const PORT = 8080;
+const PersonList = require('./p');
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(process.env.PORT||PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+});
+
+var i = 0;
+
+
+app.route("/ticket").get((req, res)=>{
+    res.send("<h1>Ticket RESTapi </h1>");
+})
+.post((req, res)=>{
+    
+    let Uid = req.body.user_id;
+    let Issue = req.body.issue;
+
+    const uniqueId = uuidv4();
+    PersonId = PersonList[i].id;
+
+    const ticket = {
+        _id: uniqueId,
+        issue: Issue,
+        Assigned_to: PersonId,
+        Raised_by: Uid
+    };
+
+    PersonList[i].ticketList.push(ticket);
+    
+    const obj = {
+        "message": "Ticket is successfully assgined to user",
+        "success": true,
+        "data": {
+            "ticket_id": uniqueId,
+            "assigned_to": PersonId
+        }
+    };
+
+    // console.log(PersonList[i]);
+    // console.log(i);
+    i = (i+1)%5;
+    // console.log(i);
+    
+    res.send(obj);
+
+});
+
+
+
---
+++ p.js
@@ -0,0 +1,35 @@
+const person1={
+    id: 1,
+    name: "person1",
+    ticketList: []
+
+};
+
+const person2={
+    id: 2,
+    name: "person2",
+    ticketList: []
+
+};
+const person3={
+    id: 3,
+    name: "person3",
+    ticketList: []
+
+};
+const person4={
+    id: 4,
+    name: "person4",
+    ticketList: []
+
+};
+const person5={
+    id: 5,
+    name: "person5",
+    ticketList: []
+
+};
+
+const ListPerson = [person1, person2, person3, person4, person5];
+
+module.exports = ListPerson;
